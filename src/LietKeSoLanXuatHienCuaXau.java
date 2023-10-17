0a3a34bfb21efbdf5666f6bea0dd494f23d761d6
tranhau510 <145999276+tranhau510@users.noreply.github.com>
2023-10-17 16:34:33 +0700

    Add files via upload

---
+++ src/LietKeSoLanXuatHienCuaXau.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LietKeSoLanXuatHienCuaXau {
+    static void DemKyTu(String str){
+        int counter[] = new int[256];
+        int len = str.length();
+        for (int i = 0; i < len; i++)
+            counter[str.charAt(i)]++;
+        char array[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            array[i] = str.charAt(i);
+            int flag = 0;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == array[j]) {
+                    flag++;
+                }
+            }
+            if (flag == 1)
+                System.out.println("Số lần xuất hiện của " + str.charAt(i) + " trong chuỗi:" + counter[str.charAt(i)]);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap 1 xau ky tu: ");
+        String str = sc.nextLine();
+        System.out.println("Chuỗi là: "+ str);
+        DemKyTu(str);
+    }
+
+}
